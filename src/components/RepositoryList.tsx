2027026ea17427791e4aad282e48bdb552d8926c
dfilastro <d.filastro@hotmail.com>
2022-02-17 15:38:16 -0300

    congured typescript

--- src/components/RepositoryList.tsx
+++ src/components/RepositoryList.tsx
@@ -3,8 +3,14 @@ import { useState, useEffect } from 'react';
 
 import '../styles/repositories.scss';
 
+interface Repository {
+  name: string;
+  description: string;
+  html_url: string;
+}
+
 export function RepositoryList() {
-  const [repositories, setRepositories] = useState([]);
+  const [repositories, setRepositories] = useState<Repository[]>([]);
 
   // Um array vazio na "dependência" do useEffet, faz com que a função seja executada uma única vez
   // Se utilizar o useEffet sem o segundo parâmetro, ele vai executar a função infinitamente
@@ -21,7 +27,7 @@ export function RepositoryList() {
 
       <ul>
         {repositories.map((repository) => {
-          return <RepositoryItem repository={repository} />;
+          return <RepositoryItem key={repository.name} repository={repository} />;
         })}
       </ul>
     </section>
